eaca3681283cc77e675743179e844f0f0f10044b
Dhruba Bhattacharyya <ece2023114@rcciit.org.in>
2025-07-21 23:59:08 +0530

    deployment fixes

--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import { sceneRouter } from './routes/scene'
 
 const app = express();
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 
 app.use('/scene', sceneRouter);
 
-app.listen(4000, () => {
-  console.log("Server is running at port 4000");
-})
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
